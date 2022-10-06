1c70b73b41f5d9fdbb2861992d9259b9de7ad0bd
spuchol81 <110041803+spuchol81@users.noreply.github.com>
2022-10-06 18:50:30 +0200

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot and Tanzu! gitops power";
+		return "Greetings from Spring Boot and Tanzu!";
 	}
 
 }
